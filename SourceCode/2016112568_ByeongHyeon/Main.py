afe30df81620b4d04e6a47f6de019dd525920a69
park1997 <iambyunghyun97@gmail.com>
2021-05-02 17:18:18 +0900

    Update Main.py

--- SourceCode/2016112568_ByeongHyeon/Main.py
+++ SourceCode/2016112568_ByeongHyeon/Main.py
@@ -16,12 +16,30 @@ title = "My Game"
 pygame.display.set_caption(title) # 창의 제목 표시줄 옵션
 # 3. 게임 내 필요한 설정
 clock = pygame.time.Clock()
+
 #RGB 값 설정
 black = (0,0,0)
 white = (255,255,255)
 k=0
 
-# 4. 메인 이벤트
+class obj:
+    def __init__(self):
+        self.x =0
+        self.y=0
+        self.move =0
+    
+
+# 충돌이 일어났는지 확인하는 함수!
+# return 값이 boolean 타입임
+def crash(a,b):
+    # 여기 범위 안에있으면 충돌이 일어났다고 판단함!
+    if(a.x-b.sx <= b.sx) and (b.x)
+
+
+
+
+
+# 4-0. 메인 이벤트
 SB=0
 # 4-1. FPS 설정
 # 4-2. 각종 입력 감지
