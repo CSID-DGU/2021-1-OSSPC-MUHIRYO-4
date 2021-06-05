b8204a9d7b996a8d4b04849a4284d57608b3773f
park1997 <iambyunghyun97@gmail.com>
2021-06-05 19:02:49 +0900

    Update Main.py

--- SourceCode/Main.py
+++ SourceCode/Main.py
@@ -3,6 +3,7 @@ import random
 import time
 from datetime import datetime
 import pygame_menu 
+from os import system
 
 pygame.init()
 infoObject = pygame.display.Info()
@@ -10,7 +11,7 @@ size = [infoObject.current_w//2,infoObject.current_h*8//9]
 screen = pygame.display.set_mode(size,pygame.RESIZABLE)
 def show_mode():
     menu.clear()
-    menu.add.button('Oasis',start_the_game1)
+    menu.add.button('Oasis',start_the_game)
     menu.add.button('Hell')
     menu.add.button('City')
     menu.add.button('Back', back)
@@ -20,21 +21,15 @@ def back():
     menu.clear()
     menu.add.button('Select mode', show_mode)
     menu.add.button('Option', show_option)
-    menu.add.button('Help',show_help)
+    menu.add.button('Help')
     menu.add.button('Quit', pygame_menu.events.EXIT)
 
 def help():
     menu.clear()
 
-def show_help():
-    menu.add.image(image_path='SourceCode/Image/howtoplay.png', angle=0, scale=(0.4, 0.4))    
-
-def sound_on_off():
-    menu.add.toggle_switch('Sound Switch', True, onchange=pygame.mixer.unpause(), toggleswitch_id='sound_switch')
-
 def show_option():
     menu.clear()
-    menu.add.button('Sound',sound_on_off)
+    menu.add.button('Sound') 
     menu.add.button('Back', back)
     menu.add.button('Quit',pygame_menu.events.EXIT)
 menu_image = pygame_menu.baseimage.BaseImage(image_path='SourceCode/Image/StartImage.png',drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL)
@@ -43,9 +38,10 @@ mytheme.background_color = menu_image
 menu = pygame_menu.Menu('MUHIRRYO GOOD', size[0], size[1], theme=mytheme)
 menu.add.button('Select mode', show_mode)
 menu.add.button('Option',show_option)
-menu.add.button('Help',show_help)
+menu.add.button('Help')
 menu.add.button('Quit',pygame_menu.events.EXIT)
 background = pygame.image.load("SourceCode/Image/StartImage.png")
-def start_the_game1():
+def start_the_game():
     import Oasis
-menu.mainloop(screen)
+menu.mainloop(screen) 
+pygame.quit()
